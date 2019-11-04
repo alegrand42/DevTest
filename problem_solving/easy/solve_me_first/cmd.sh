5dc0ed8c3be8f9e4a1f1e951d0ef0525ff4b9bb3
thanquol <arltcycle@gmail.com>
2019-11-04 22:12:06 +0100

    go: solve me first

--- problem_solving/easy/solve_me_first/cmd.sh
+++ problem_solving/easy/solve_me_first/cmd.sh
@@ -1,4 +1,11 @@
+# Ruby
 echo 'check right on file'
 chmod 750 solve_me_first.rb
 echo 'please enter 2 var separated by enter'
 ./solve_me_first.rb
+
+# Go
+go run solve_me_first.go
+
+# Elixir
+elixir solve_me_first.exs
